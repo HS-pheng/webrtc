450a78da7fe86a9982cbd56b258a4303a56389f5
mango-martin <martin@mango-byte.com>
2022-10-12 14:11:30 +0700

    chore: add windicss

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>{{ msg }}</div>
-  <h2>Hello World!!</h2>
+  <div class="flex flex-col items-center w-[400px] mx-auto my-20 border-2">
+    <h1>Socket connected: {{ ws.connected }}</h1>
+    <h2>Socket ID: {{ id }}</h2>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useWebsocket } from '~/stores/useWebsocket';
+import { useWebsocket } from '../stores/useWebsocket';
 
-const msg = 'Hello World!';
-const { connect } = useWebsocket();
+definePageMeta({
+  layout: 'default',
+});
+
+const ws = useWebsocket();
 
 onMounted(() => {
-  connect();
+  ws.connect();
 });
+
+watch(
+  () => ws.connected,
+  (value) => {
+    if (value) {
+      ws.socket.emit('join', 'test');
+    }
+  },
+);
+
+const id = computed(() => ws.socket?.id || '-');
 </script>
--- client/stores/useWebsocket.ts
+++ client/stores/useWebsocket.ts
@@ -3,12 +3,15 @@ import { Socket, io } from 'socket.io-client';
 
 export const useWebsocket = defineStore('socket', () => {
   const socket = ref<Socket | null>(null);
+  const connected = ref(false);
 
   const connect = () => {
     socket.value = io('http://localhost:3001', { autoConnect: false });
     socket.value.on('connect', () => {
-      console.log('connected');
+      console.log('socket connected');
+      connected.value = true;
     });
+    socket.value.on('joined', console.log);
     socket.value.connect();
   };
 
@@ -19,6 +22,7 @@ export const useWebsocket = defineStore('socket', () => {
 
   return {
     socket,
+    connected,
     connect,
     disconnect,
   };
